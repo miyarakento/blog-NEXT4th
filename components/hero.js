8df731b2c025e8eb7898f33623b8ba90339efb42
miyarakento <n23031@std.it-college.ac.jp>
2023-12-19 06:39:56 +0000

    d

--- components/hero.js
+++ components/hero.js
@@ -1,6 +1,15 @@
-export default function Hero( {
-  return <h1>CUBE</h1>
-         <p>kj</p>
-      </div>
-)
-}
+import styles from 'styles/hero.module.css'
++ 
++  const Hero = ({ title, subtitle, imageOn = false }) => {
+     return (
+~    ┆ <div className={styles.text}>
+~    ┆ ┆ <h1 className={styles.title}>{title}</h1>
+~    ┆ ┆ <p className={styles.subtitle}>{subtitle}</p>
+~    ┆ ┆ <h1>{title}</h1>
++    ┆ ┆ <p>{subtitle}</p>
++    ┆ ┆ {imageOn && <figure> [画像] </figure>}
+     ┆ </div>
+     )
+   }
++  
+    export default Hero
---
+++ components/nav.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import styles from 'styles/nav.module.css'
+
+export default fanction Nav() {
+	return (
+	 <nav>
+	   <ul className={style.list}>
+	     <li>
+		<Link href="/">
+		 <a>Home</a>
+	        </Link>
+	     </li>
+	     <li>
+
+	    </li>
+	    <li>
+	    </li>
+	  </ul>
+	 </nav> 	
+	)
+}	
